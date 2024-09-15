e30dc76e78cf600e548ad0e060822a5fe4694de0
Souvik Bera <souvikbera020@gmail.com>
2024-09-16 02:32:31 +0530

    first commit

--- client/src/components/pages/HomePage.jsx
+++ client/src/components/pages/HomePage.jsx
@@ -5,32 +5,38 @@ function HomePage() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-pink-100">
-      <div className="rounded-lg shadow-lg bg-gradient-to-b from-cyan-200 to-purple-100 w-11/12 sm:w-96 mt-10 mb-8 p-4 sm:p-6 h-[90vh] sm:h-[85vh] overflow-hidden">
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-yellow-100 to-green-100">
+      <div className="rounded-lg shadow-lg bg-gradient-to-b from-blue-200 to-purple-200 w-11/12 sm:w-96 mt-10 mb-8 p-4 sm:p-6 h-[90vh] sm:h-[85vh] overflow-hidden">
         <div className="text-center mb-6 sm:mb-8">
-          <h1 className="text-2xl sm:text-4xl font-bold text-white">Fruit.Ai</h1>
-          <p className="text-base sm:text-lg pb-8 font-bold text-white">"Be Healthy!"</p>
+          <h1 className="text-2xl sm:text-4xl font-bold text-gray-800">Fruit.Ai</h1>
+          <p className="text-base sm:text-lg pb-8 font-bold text-gray-700">"Be Healthy!"</p>
         </div>
         <div className="grid grid-cols-2 gap-4">
           <button
-            className="bg-yellow-200 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-purple-600"
+            className="bg-indigo-300 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-gray-800"
             onClick={() => navigate('/chatbot')} 
           >
             Chat
           </button>
-          <button className="bg-purple-400 rounded-lg shadow-lg blur-md px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold"></button>
-          <button className="bg-pink-200 rounded-lg shadow-lg blur-md px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-purple-600"></button>
-          <button className="bg-green-100 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-cyan-700 flex items-center justify-center"
+          <button className="bg-blue-300 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-gray-800"
+          >
+            Option 1
+          </button>
+          <button className="bg-red-300 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-gray-800"
+          >
+            Option 2
+          </button>
+          <button className="bg-green-300 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-gray-800 flex items-center justify-center"
           onClick={() => navigate('/translator')} 
           >
             Translate
           </button>
-          <button className="bg-yellow-100 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-blue-800"
+          <button className="bg-orange-300 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-gray-800"
           onClick={() => navigate('/faq')}
           >
             FAQs
           </button>
-          <button className="bg-pink-100 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-pink-700"
+          <button className="bg-pink-300 rounded-lg shadow-lg px-4 sm:px-6 py-3 h-[12vh] sm:h-[16vh] text-lg sm:text-2xl font-bold text-gray-800"
           onClick={() => navigate('/about')}
           >
             About
--- client/src/components/pages/LoginPage.jsx
+++ client/src/components/pages/LoginPage.jsx
@@ -30,26 +30,28 @@ function LoginPage() {
   };
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-yellow-50">
-      <div className="bg-white border-2 border-gray-200 shadow-xl rounded-2xl grid-cols-1 lg:grid-cols-2">
+    <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-br from-green-200 to-yellow-100">
+      <div className="bg-white border-2 border-blue-300 shadow-xl rounded-2xl grid-cols-1 lg:grid-cols-2">
         <div className="flex flex-col justify-center p-6 lg:p-12">
-          <h2 className="text-2xl lg:text-4xl text-center mb-6">
+          <h2 className="text-2xl lg:text-4xl text-center mb-6 text-gray-800">
             {activeTab === "login" ? "Login" : "Register"}
           </h2>
-          <p className="text-center mb-6 text-gray-500 text-sm lg:text-base">
-            {activeTab === "login"
-              ? <>
-                  By signing in you are agreeing to our
-                  <a href="#" className="text-blue-500 hover:underline ml-1">
-                    Terms and Privacy Policy
-                  </a>
-                </>
-              : <>
-                  By signing up, you agree to our
-                  <a href="#" className="text-blue-500 hover:underline ml-1">
-                    Terms and Privacy Policy
-                  </a>
-                </>}
+          <p className="text-center mb-6 text-gray-600 text-sm lg:text-base">
+            {activeTab === "login" ? (
+              <>
+                By signing in you are agreeing to our
+                <a href="#" className="text-purple-500 hover:underline ml-1">
+                  Terms and Privacy Policy
+                </a>
+              </>
+            ) : (
+              <>
+                By signing up, you agree to our
+                <a href="#" className="text-purple-500 hover:underline ml-1">
+                  Terms and Privacy Policy
+                </a>
+              </>
+            )}
           </p>
 
           <div className="flex items-center justify-center w-full">
@@ -59,7 +61,7 @@ function LoginPage() {
                   href="#"
                   className={`text-opacity-90 border-b-2 py-2 transition-all ${
                     activeTab === "login"
-                      ? "text-blue-500 border-black font-semibold"
+                      ? "text-purple-500 border-purple-500 font-semibold"
                       : "text-gray-400 border-transparent"
                   }`}
                   onClick={() => setActiveTab("login")}
@@ -70,7 +72,7 @@ function LoginPage() {
                   href="#"
                   className={`text-opacity-90 border-b-2 py-2 transition-all ${
                     activeTab === "register"
-                      ? "text-blue-500 border-black font-semibold"
+                      ? "text-purple-500 border-purple-500 font-semibold"
                       : "text-gray-400 border-transparent"
                   }`}
                   onClick={() => setActiveTab("register")}
@@ -83,14 +85,14 @@ function LoginPage() {
                   <input
                     placeholder="Email Address"
                     type="email"
-                    className="border-b-2 focus:outline-none p-2"
+                    className="border-b-2 focus:outline-none p-2 bg-purple-50"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                   />
                   <input
                     placeholder="Password"
                     type="password"
-                    className="border-b-2 focus:outline-none p-2"
+                    className="border-b-2 focus:outline-none p-2 bg-purple-50"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
@@ -99,12 +101,12 @@ function LoginPage() {
                       <input type="checkbox" className="mr-2" />
                       Remember me
                     </div>
-                    <a href="#" className="text-blue-500 hover:underline">
+                    <a href="#" className="text-purple-500 hover:underline">
                       Forgot Password?
                     </a>
                   </div>
                   <button
-                    className="rounded p-2 bg-blue-500 text-white hover:bg-blue-600"
+                    className="rounded p-2 bg-purple-500 text-white hover:bg-purple-600"
                     onClick={handleLogin}
                   >
                     Login
@@ -114,28 +116,30 @@ function LoginPage() {
                     <div className="flex justify-center gap-4 mt-2">
                       <a
                         href="#"
-                        className="text-gray-600 hover:text-blue-500"
+                        className="text-gray-600 hover:text-purple-500"
                       >
                         <i className="fab fa-facebook-f fa-lg"></i>
                       </a>
                       <a
                         href="#"
-                        className="text-gray-600 hover:text-blue-500"
+                        className="text-gray-600 hover:text-purple-500"
                       >
                         <i className="fab fa-instagram fa-lg"></i>
                       </a>
                       <a
                         href="#"
-                        className="text-gray-600 hover:text-blue-500"
+                        className="text-gray-600 hover:text-purple-500"
                       >
                         <i className="fab fa-linkedin-in fa-lg"></i>
                       </a>
                     </div>
                     <div className="text-center mt-4">
-                      <button className="rounded p-2 bg-blue-300 text-blue-700 hover:bg-gray-400">
+                      <button className="rounded p-2 bg-purple-300 text-purple-700 hover:bg-gray-400">
                         <i className="fas fa-fingerprint fa-lg"></i>
                       </button>
-                        <p className="text-gray-300 py-2">email-user@example.com && password-password123</p>
+                      <p className="text-gray-300 py-2">
+                        email-user@example.com && password-password123
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -146,21 +150,21 @@ function LoginPage() {
                   <input
                     placeholder="Email Address"
                     type="email"
-                    className="border-b-2 focus:outline-none p-2"
+                    className="border-b-2 focus:outline-none p-2 bg-blue-50"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                   />
                   <input
                     placeholder="Password"
                     type="password"
-                    className="border-b-2 focus:outline-none p-2"
+                    className="border-b-2 focus:outline-none p-2 bg-blue-50"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                   />
                   <input
                     placeholder="Confirm Password"
                     type="password"
-                    className="border-b-2 focus:outline-none p-2"
+                    className="border-b-2 focus:outline-none p-2 bg-blue-50"
                     value={confirmPassword}
                     onChange={(e) => setConfirmPassword(e.target.value)}
                   />
